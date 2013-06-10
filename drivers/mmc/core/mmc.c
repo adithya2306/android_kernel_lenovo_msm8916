678469bafba2582e095cd5741e4dc6f6dc4b06fd
Ulf Hansson <ulf.hansson@linaro.org>
2013-06-10 17:03:43 +0200

    mmc: core: Handle both poweroff notification types for eMMC
    Depending on the context of the operation while powering down the card,
    either POWER_OFF_NOTIFY_SHORT or POWER_OFF_NOTIFY_LONG will be used. In
    suspend context a short timeout is preferred while a long timeout would
    be acceptable in a shutdown/hibernation context.
    
    We add a new parameter to the mmc_suspend function so we can provide an
    indication of what notification type to use.
    
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    Signed-off-by: Chris Ball <cjb@laptop.org>
    Git-commit: bb09907fdbc4451448e9efb9009f0ef538e52861
    Git-repo: https://android.googlesource.com/kernel/common.git
    [imaund@codeaurora.org: Resolved context conflicts]
    Signed-off-by: Ian Maund <imaund@codeaurora.org>
    Signed-off-by: dev-harsh1998 <harshitjain6751@gmail.com>

--- drivers/mmc/core/mmc.c
+++ drivers/mmc/core/mmc.c
@@ -1851,10 +1851,7 @@ static void mmc_detect(struct mmc_host *host)
 	}
 }
 
-/*
- * Suspend callback from host.
- */
-static int mmc_suspend(struct mmc_host *host)
+static int _mmc_suspend(struct mmc_host *host, bool is_suspend)
 {
 	int err = 0;
 
@@ -1883,6 +1880,14 @@ static int mmc_suspend(struct mmc_host *host)
 out:
 	mmc_release_host(host);
 	return err;
+}
+
+/*
+ * Suspend callback from host.
+ */
+static int mmc_suspend(struct mmc_host *host)
+{
+	return _mmc_suspend(host, true);
 }
 
 /*
