f99ce3fe5c922da337ed81d8333993ffb41c6608
Joe Maples <joe@frap129.org>
2017-05-10 21:44:21 -0500

    Update State_Notifier Driver
    Signed-off-by: Joe Maples <joe@frap129.org>
    Signed-off-by: Shoaib0597 <Shoaib0595@gmail.com>
    
    mdss: Add state_notifier hooks
    
    Signed-off-by: Joe Maples <joe@frap129.org>
    Signed-off-by: Shoaib0597 <Shoaib0595@gmail.com>

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -31,6 +31,10 @@
 #include <linux/powersuspend.h>
 #endif
 
+#ifdef CONFIG_STATE_NOTIFIER
+#include <linux/state_notifier.h>
+#endif
+
 #define DT_CMD_HDR 6
 
 /* NT35596 panel specific status variables */
@@ -624,6 +628,10 @@ static int mdss_dsi_panel_on(struct mdss_panel_data *pdata)
 
 	pr_debug("%s: ctrl=%pK ndx=%d\n", __func__, ctrl, ctrl->ndx);
 
+	#ifdef CONFIG_STATE_NOTIFIER
+	       state_resume();
+	#endif
+
 	if (pinfo->dcs_cmd_by_left) {
 		if (ctrl->ndx != DSI_CTRL_LEFT)
 			goto end;
@@ -751,6 +759,10 @@ static int mdss_dsi_panel_off(struct mdss_panel_data *pdata)
 
 	pr_debug("%s: ctrl=%pK ndx=%d\n", __func__, ctrl, ctrl->ndx);
 
+	#ifdef CONFIG_STATE_NOTIFIER
+	       state_suspend();
+	#endif
+
 	if (pinfo->dcs_cmd_by_left) {
 		if (ctrl->ndx != DSI_CTRL_LEFT)
 			goto end;
@@ -792,6 +804,11 @@ static int mdss_dsi_panel_low_power_config(struct mdss_panel_data *pdata,
 		pinfo->blank_state = MDSS_PANEL_BLANK_LOW_POWER;
 	else
 		pinfo->blank_state = MDSS_PANEL_BLANK_UNBLANK;
+
+        #ifdef CONFIG_STATE_NOTIFIER
+	if (enable)
+	   state_suspend();
+        #endif
 
 	pr_debug("%s:-\n", __func__);
 	return 0;
