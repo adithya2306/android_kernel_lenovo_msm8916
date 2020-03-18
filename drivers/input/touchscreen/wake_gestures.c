c494b7603daec0207b46e6e3f31112a1ac6fc8f7
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-03-18 16:16:17 +0530

    wake_gestures: Enable DT2W by default
    - the toggle in settings is enabled by default so we obviously
      gotta keep it enabled from the kernel too

--- drivers/input/touchscreen/wake_gestures.c
+++ drivers/input/touchscreen/wake_gestures.c
@@ -39,7 +39,7 @@
 /* Tuneables */
 #define WG_DEBUG		0
 #define WG_DEFAULT		0
-#define DT2W_DEFAULT		0
+#define DT2W_DEFAULT		1
 #define S2W_DEFAULT		0
 #define S2S_DEFAULT		0
 #define WG_PWRKEY_DUR           60
